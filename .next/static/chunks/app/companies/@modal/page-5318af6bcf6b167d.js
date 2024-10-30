e09ab1c900a1e0c79aba82f0af9dcf6bcbb0706b
DmytroMykolaichuk <dmitriymikolaychuk@gmail.com>
2024-10-30 15:51:27 +0200

    fix

---
+++ .next/static/chunks/app/companies/@modal/page-5318af6bcf6b167d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[440,160,409,931,836,615,529,609,957,47,183,570,710,150,538,855,702,663],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
---
+++ .next/static/chunks/app/dashboard/page-785afd39e456b372.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[702,160,409,931,836,615,529,609,440,957,47,183,570,710,150,538,855,663],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
